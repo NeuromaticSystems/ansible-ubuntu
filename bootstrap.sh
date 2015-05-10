08934ae3ad234421eba893cf75d61bc4ced92788
Brian Choate <brianchoate@users.noreply.github.com>
2015-05-09 20:13:58 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y python-setuptools
 sudo sudo easy_install pip
 sudo pip install battleschool
 
-battleschool --ask-sudo-pass --update-sources  --config-file https://raw.githubusercontent.com/NeuromaticSystems/ansible-ubuntu/master/config.yml
+battle --ask-sudo-pass --update-sources  --config-file https://raw.githubusercontent.com/NeuromaticSystems/ansible-ubuntu/master/config.yml
 
 
 # EOF ############################################################################
